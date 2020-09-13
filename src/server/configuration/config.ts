8984b84d5e7669c2c0f913349deb19c8da47a57e
Carbon <ramonpaw@gmail.com>
2020-09-13 20:58:37 +0200

    Config Rewrite

--- src/server/configuration/config.ts
+++ src/server/configuration/config.ts
@@ -6,42 +6,48 @@ export const CONFIG = {
       y: -787.5165,
       z: 30.0212,
    },
-   // VEHICLE
-   vehiclePlateName: "MW",
-   // LOBBY
-   minPlayer: 0,
-   prepTimer: 1000 * 15,
-   unprepTimer: 1000 * 60,
-   initTimer: 1000 * 5,
+
+   VEHICLE: {
+      PLATENAME: "MW",
+   },
+   LOBBY: {
+      MINPLAYER: 0,
+      PREPTIME: 1000 * 15,
+      UNPREPTIME: 1000 * 60,
+      INITTIME: 1000 * 5,
+   },
+   WEATHER: {
+      CYCLETIME: 1000 * 5,
+      CYCLES: [
+         0, // Extra sunny
+         0, // Extra sunny
+         0, // Extra sunny
+         0, // Extra sunny
+         0, // Extra sunny
+         0, // Extra sunny
+         0, // Extra sunny
+         1, // Clear
+         2, // Clouds
+         2, // Clouds
+         4, // Foggy
+         5, // Overcast
+         8, // Light Rain
+         6, // Rain
+         6, // Rain
+         7, // Thunder
+         7, // Thunder
+         6, // Rain
+         8, // Light Rain
+         5, // Overcast
+         2, // Clouds
+         1, // Clear
+         1, // Clear
+      ],
+   },
    // WEATHER
-   weatherCycleTime: 1000 * 5,
-   weatherCycle: [
-      0, // Extra sunny
-      0, // Extra sunny
-      0, // Extra sunny
-      0, // Extra sunny
-      0, // Extra sunny
-      0, // Extra sunny
-      0, // Extra sunny
-      1, // Clear
-      2, // Clouds
-      2, // Clouds
-      4, // Foggy
-      5, // Overcast
-      8, // Light Rain
-      6, // Rain
-      6, // Rain
-      7, // Thunder
-      7, // Thunder
-      6, // Rain
-      8, // Light Rain
-      5, // Overcast
-      2, // Clouds
-      1, // Clear
-      1, // Clear
-   ],
-   // Time
-   startHour: 6, // What time do we start the server at?
-   minutesPerMinute: 5, // Used to set the Time Scale of the server for the minute. 1 Minute = 30 Minutes.
-   hoursPerSixtyMinutes: 1, // Used to set the Time Scale of the server for the hour.
+   TIME: {
+      STARTHOUR: 6, // What time do we start the server at?
+      PERMINUTE: 5, // Used to set the Time Scale of the server for the minute. 1 Minute = 30 Minutes.
+      TIMESCALE: 1, // Used to set the Time Scale of the server for the hour.
+   },
 };
